4ecde05361467ca7059e78aa584f3822d625cf11
Santiago Rincon <rincorpes@gmail.com>
2019-11-12 11:58:17 -0500

    Hello, World

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Hello World with Python
+
+"""
+  * Print "Hello, World!" on the console using `print`
+  *
+  * Test: open the console, follow this file path nad run `py ./helloworld.py`
+  """
+print("Hello, World!")
